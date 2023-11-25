04abb76fa72a270d4a2a61dd9f8757b01386823a
mtwhaley <mitchell.t.whaley@gmail.com>
2023-11-24 22:00:00 -0600

    Category Selection Feature Enabled

--- ScriptSheets/General.js
+++ ScriptSheets/General.js
@@ -1,3 +1,72 @@
+async function filterCategories(catID) {
+    const categoryClicker=document.getElementById(catID)
+    ResetCategoryBackgrounds()
+    categoryClicker.style.backgroundColor="#edb826"
+
+    const productDiv=document.getElementById("productSpace")
+    const elements=productDiv.childNodes
+    for (var i=0;i<elements.length;i++) {
+        if(elements[i].nodeName=="DIV") {   //cards
+            let itemNumber= await getCardNumber(elements[i])
+            const item=await searchJSON(itemNumber)
+            let check=itemCategoryCheck(item, catID)
+            if (check==true) {
+                elements[i].style.display="block"
+            }
+            else {
+                elements[i].style.display="none"
+            }
+        }
+    };
+    console.log("Here")
+
+}
+
+function itemCategoryCheck(item,category) {
+    switch (category) {
+        case "Decor":
+            return item.Decor
+        case "Apparel":
+            return item.Apparel
+        case "Stuffed":
+            return item.Stuffed
+        case "Misc":
+            return item.Misc
+        case "Seasonal":
+            return item.Seasonal
+        case "Themed":
+            return item.Themed
+        case "Slime":
+            return item.Slime
+        
+        default:
+            return false
+    }
+}
+
+function getCardNumber(card) {
+    return new Promise((resolve, reject)=> {
+        const elements=card.childNodes
+    elements.forEach(element => {
+        if (element.nodeName=="IMG") {
+            let id=element.id
+            number=id.substring(3)
+            resolve(number)
+        }
+    });
+    })
+    
+}
+
+function ResetCategoryBackgrounds() {
+    const div=document.getElementById("categoryText")
+    const elements=div.childNodes
+    elements.forEach(child => {
+        if (child.nodeName=="BUTTON") {
+            child.style.backgroundColor=""
+        }
+    });
+}
 
 function createAllProductCards() {
     JSONData.forEach(item => {
@@ -15,11 +84,11 @@ function createProductCard(item) {
     const image=document.createElement("img")
     let source="../Product Images/"+item.Photo_File_Name+".jpg"
     image.src=source
+    let id="img"+item.Item_Number
+    image.setAttribute("id", id)
     image.setAttribute("class","productimage")
 
     const text=document.createElement("p")
-    console.log(item)
-    console.log(item.Price)
     if (item.Price==null) {
         item.Price=0
     }
